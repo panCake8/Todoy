e1b7e7c22781a55df531c586bcaeea0435b6973e
ay0o5h <aayosh553@gmail.com>
2023-04-08 02:36:08 +0300

    add api functions

---
+++ app/src/main/java/com/tahaproject/todoy_app/util/EndPoint.kt
@@ -0,0 +1,8 @@
+package com.tahaproject.todoy_app.util
+
+object EndPoint {
+    const val  personalTodo="todo/personal"
+    const val  signup= "signup"
+    const val  login="login"
+    const val  teamTodo="todo/team"
+}
