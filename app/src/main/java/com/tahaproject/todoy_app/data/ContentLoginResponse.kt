e308845f9d23fcae590b4c4425aabb607e32c70b
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-04-08 00:16:13 +0300

    make the data classes as serializable

--- app/src/main/java/com/tahaproject/todoy_app/data/ContentLoginResponse.kt
+++ app/src/main/java/com/tahaproject/todoy_app/data/ContentLoginResponse.kt
@@ -1,6 +1,9 @@
 package com.tahaproject.todoy_app.data
 
+import com.google.gson.annotations.SerializedName
+
 data class ContentLoginResponse(
-    val token: String?,
-    val expireAt: String?,
+    @SerializedName("token") val token: String?,
+    @SerializedName("expireAt") val expireAt: String?,
 )
+
