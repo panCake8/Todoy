7ddf1470e16ca1edcddfa2b5d6ad19fdab4193c0
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-04-07 17:46:34 +0200

    add base fragment

---
+++ app/src/main/java/com/tahaproject/todoy_app/ui/baseview/BaseFragment.kt
@@ -0,0 +1,28 @@
+package com.tahaproject.todoy_app.ui.baseview
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB : ViewBinding> : Fragment() {
+    private var _binding: VB? = null
+    private val binding get() = _binding!!
+    abstract val bindingInflate: (LayoutInflater, ViewGroup?, Boolean) -> VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bindingInflate(layoutInflater, container, false)
+        return requireNotNull(_binding).root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
