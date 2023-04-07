bb07f5dcf6fba56d9dcc788629a303797c13bb0e
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-04-08 00:05:31 +0300

    add failure response

---
+++ app/src/main/java/com/tahaproject/todoy_app/data/FailureResponse.kt
@@ -0,0 +1,13 @@
+package com.tahaproject.todoy_app.data
+
+data class FailureResponse(
+    val value: ContentLoginResponse?,
+    val message: String?,
+    val isSuccess: Boolean?
+)
+
+//{
+//    "value": null,
+//    "message": "invalid teamId, please use a correct provided teamId by uncle Bareq",
+//    "isSuccess": false
+//}
