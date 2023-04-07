90e22c94ab42d9318d403c5a5201b6c8537fc59b
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-04-08 01:40:43 +0300

    add another requests

--- app/src/main/java/com/tahaproject/todoy_app/data/responses/PersonalTodoCreateResponse.kt
+++ app/src/main/java/com/tahaproject/todoy_app/data/responses/PersonalTodoCreateResponse.kt
@@ -7,36 +7,3 @@ data class PersonalTodoCreateResponse(
     @SerializedName("message")  val message :String?,
     @SerializedName("isSuccess")  val isSuccess :Boolean?
 )
-
-
-data class LoginRequest(
-    val username: String,
-    val password: String
-)
-
-/*
-val client = OkHttpClient()
-
-// create a LoginRequest object with the username and password
-val loginRequest = LoginRequest("myusername", "mypassword")
-
-// convert the LoginRequest object to a JSON string
-val requestBody = Gson().toJson(loginRequest)
-
-// create a new request with the JSON string as the request body
-val request = Request.Builder()
-    .url("https://example.com/login")
-    .post(requestBody.toRequestBody(MediaType.parse("application/json")))
-    .build()
-
-// execute the request and handle the response
-client.newCall(request).enqueue(object : Callback {
-    override fun onFailure(call: Call, e:   IOException) {
-        // handle the error
-    }
-
-    override fun onResponse(call: Call, response: Response) {
-        // handle the response
-    }
-})
-*/
