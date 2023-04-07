9d148050a82ec50db10f7903189b0c6406232b8d
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-04-08 01:12:14 +0300

    add team todo data class

---
+++ app/src/main/java/com/tahaproject/todoy_app/data/TeamToDo.kt
@@ -0,0 +1,12 @@
+package com.tahaproject.todoy_app.data
+
+import com.google.gson.annotations.SerializedName
+
+data class TeamToDo(
+    @SerializedName("id")   val id: String?,
+    @SerializedName("title")   val title:String?,
+    @SerializedName("assignee")   val title:String?,
+    @SerializedName("description")   val description:String?,
+    @SerializedName("status")   val status:Int?,
+    @SerializedName("creationTime")   val creationTime:String?
+)
