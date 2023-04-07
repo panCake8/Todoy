ab90bd2e7fc56dc491ad38b7d4288e6b8f6c0fe0
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-04-08 00:10:02 +0300

    Update FailureResponse.kt

--- app/src/main/java/com/tahaproject/todoy_app/data/FailureResponse.kt
+++ app/src/main/java/com/tahaproject/todoy_app/data/FailureResponse.kt
@@ -5,9 +5,3 @@ data class FailureResponse(
     val message: String?,
     val isSuccess: Boolean?
 )
-
-//{
-//    "value": null,
-//    "message": "invalid teamId, please use a correct provided teamId by uncle Bareq",
-//    "isSuccess": false
-//}
