7b1bbbc8c97637b549e6e748a399435757df34e2
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-04-08 02:22:30 +0300

    add fake token

--- app/src/main/java/com/tahaproject/todoy_app/util/Constants.kt
+++ app/src/main/java/com/tahaproject/todoy_app/util/Constants.kt
@@ -2,4 +2,6 @@ package com.tahaproject.todoy_app.util
 
 object Constants {
     const val url = "https://team-todo-62dmq.ondigitalocean.app"
+    const val token =
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJodHRwczovL3RoZS1jaGFuY2Uub3JnLyIsInN1YiI6IjdmOGZlOTY2LWQzYjItNDk1My1iNDBkLWY0YzAwNmM1YTUwMCIsInRlYW1JZCI6ImYwOTdmOTQzLWU0YTMtNDZmNC04NDAyLTdjOWI1NjUzMzc1NiIsImlzcyI6Imh0dHBzOi8vdGhlLWNoYW5jZS5vcmcvIiwiZXhwIjoxNjgxMDczNTMyfQ.RQ2ap5KFtsZC1ienRVze8BbhtvSNRH0PV6BOPeQYrrQ"
 }
