b5ea66b38aa385cecdc1a5d615ca7f3f40720aeb
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-04-07 17:46:54 +0200

    add base home activity

---
+++ app/src/main/java/com/tahaproject/todoy_app/ui/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.tahaproject.todoy_app.ui
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.tahaproject.todoy_app.R
+import com.tahaproject.todoy_app.databinding.ActivityHomeBinding
+import com.tahaproject.todoy_app.ui.baseview.BaseActivity
+
+class HomeActivity : BaseActivity<ActivityHomeBinding>() {
+
+    override val bindingInflater: (LayoutInflater) -> ActivityHomeBinding
+        get() = ActivityHomeBinding::inflate
+}
---
+++ app/src/main/java/com/tahaproject/todoy_app/util/ExFucntions.kt
@@ -0,0 +1,2 @@
+package com.tahaproject.todoy_app.util
+
