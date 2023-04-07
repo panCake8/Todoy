02ef931a1619a135063ade2e36468d8b1f98715c
ay0o5h <aayosh553@gmail.com>
2023-04-08 00:36:51 +0300

    parsing personal data

--- app/src/main/java/com/tahaproject/todoy_app/data/PeronalTodoResponse.kt
+++ app/src/main/java/com/tahaproject/todoy_app/data/PeronalTodoResponse.kt
@@ -1,3 +1,9 @@
 package com.tahaproject.todoy_app.data
 
-data class PeronalTodoResponse()
+import com.google.gson.annotations.SerializedName
+
+data class PersonalTodoCreateResponse(
+    @SerializedName("value")  val value :PersonalTodo?,
+    @SerializedName("message")  val message :String?,
+    @SerializedName("isSuccess")  val isSuccess :Boolean?
+)
--- app/src/main/java/com/tahaproject/todoy_app/data/PersonalTodoUpdateResponse.kt
+++ app/src/main/java/com/tahaproject/todoy_app/data/PersonalTodoUpdateResponse.kt
@@ -1,3 +1,9 @@
 package com.tahaproject.todoy_app.data
 
-data class PersonalTodoUpdateResponse()
+import com.google.gson.annotations.SerializedName
+
+data class PersonalTodoUpdateResponse(
+    @SerializedName("value") val  value:String?,
+    @SerializedName("message")  val message :String?,
+    @SerializedName("isSuccess")  val isSuccess :Boolean?
+)
