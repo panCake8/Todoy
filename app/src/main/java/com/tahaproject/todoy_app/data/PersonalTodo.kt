364e9feeaa25283d9600eb640da2597f07a6aca4
ay0o5h <aayosh553@gmail.com>
2023-04-08 00:36:51 +0300

    parsing personal data

--- app/src/main/java/com/tahaproject/todoy_app/data/PersonalTodo.kt
+++ app/src/main/java/com/tahaproject/todoy_app/data/PersonalTodo.kt
@@ -1,3 +1,11 @@
 package com.tahaproject.todoy_app.data
 
-data class PersonalTodo()
+import com.google.gson.annotations.SerializedName
+
+data class PersonalTodo(
+    @SerializedName("id")   val id: String?,
+    @SerializedName("title")   val title:String?,
+    @SerializedName("description")   val description:String?,
+    @SerializedName("status")   val status:Int?,
+    @SerializedName("creationTime")   val creationTime:String?
+)
