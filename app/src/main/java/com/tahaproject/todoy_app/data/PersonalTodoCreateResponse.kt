d93d66351e7d36986075734442ff7c228ab6a06b
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-04-08 01:24:29 +0300

    change file name

---
+++ app/src/main/java/com/tahaproject/todoy_app/data/PersonalTodoCreateResponse.kt
@@ -0,0 +1,42 @@
+package com.tahaproject.todoy_app.data
+
+import com.google.gson.annotations.SerializedName
+
+data class PersonalTodoCreateResponse(
+    @SerializedName("value")  val value :PersonalTodo?,
+    @SerializedName("message")  val message :String?,
+    @SerializedName("isSuccess")  val isSuccess :Boolean?
+)
+
+
+data class LoginRequest(
+    val username: String,
+    val password: String
+)
+
+/*
+val client = OkHttpClient()
+
+// create a LoginRequest object with the username and password
+val loginRequest = LoginRequest("myusername", "mypassword")
+
+// convert the LoginRequest object to a JSON string
+val requestBody = Gson().toJson(loginRequest)
+
+// create a new request with the JSON string as the request body
+val request = Request.Builder()
+    .url("https://example.com/login")
+    .post(requestBody.toRequestBody(MediaType.parse("application/json")))
+    .build()
+
+// execute the request and handle the response
+client.newCall(request).enqueue(object : Callback {
+    override fun onFailure(call: Call, e:   IOException) {
+        // handle the error
+    }
+
+    override fun onResponse(call: Call, response: Response) {
+        // handle the response
+    }
+})
+*/
--- app/src/main/java/com/tahaproject/todoy_app/data/TeamToDo.kt
+++ app/src/main/java/com/tahaproject/todoy_app/data/TeamToDo.kt
@@ -3,10 +3,9 @@ package com.tahaproject.todoy_app.data
 import com.google.gson.annotations.SerializedName
 
 data class TeamToDo(
-    @SerializedName("id")   val id: String?,
-    @SerializedName("title")   val title:String?,
-    @SerializedName("assignee")   val title:String?,
-    @SerializedName("description")   val description:String?,
-    @SerializedName("status")   val status:Int?,
-    @SerializedName("creationTime")   val creationTime:String?
+    @SerializedName("id") val id: String?,
+    @SerializedName("assignee") val title: String?,
+    @SerializedName("description") val description: String?,
+    @SerializedName("status") val status: Int?,
+    @SerializedName("creationTime") val creationTime: String?
 )
