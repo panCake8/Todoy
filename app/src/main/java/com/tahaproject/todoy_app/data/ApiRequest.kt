81f473f65acbc6cc7e475030d53aea41f1893747
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-04-08 02:19:03 +0300

    add api requests

---
+++ app/src/main/java/com/tahaproject/todoy_app/data/ApiRequest.kt
@@ -0,0 +1,66 @@
+package com.tahaproject.todoy_app.data
+
+import com.google.gson.Gson
+import com.tahaproject.todoy_app.data.requests.LoginRequest
+import com.tahaproject.todoy_app.data.responses.LogInResponse
+import com.tahaproject.todoy_app.util.Constants
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.IOException
+
+class ApiRequest(private val gson: Gson) : IRequestApis {
+
+    private val client = OkHttpClient()
+    private val token = ""
+    private fun postRequest(body: Any, endPoint: String): Request {
+        return Request
+            .Builder()
+            .url("${Constants.url}/$endPoint")
+            .post(
+                Gson().toJson(body).toRequestBody("application/json".toMediaTypeOrNull())
+            )
+            .header("Authorization", "Barear $token")
+            .build()
+    }
+
+    private fun getRequest(endPoint: String): Request {
+        return Request
+            .Builder()
+            .url("${Constants.url}/$endPoint")
+            .get()
+            .header("Authorization", "Barear $token")
+            .build()
+    }
+
+
+    override fun login() {
+    }
+
+    override fun register() {
+    }
+
+    override fun createPersonalTodo() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPersonalTodos() {
+        TODO("Not yet implemented")
+    }
+
+    override fun updatePersonalTodosStatus() {
+        TODO("Not yet implemented")
+    }
+
+    override fun createTeamTodo() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getTeamTodos() {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateTeamTodosStatus() {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/com/tahaproject/todoy_app/util/Constants.kt
+++ app/src/main/java/com/tahaproject/todoy_app/util/Constants.kt
@@ -1,6 +1,7 @@
 package com.tahaproject.todoy_app.util
 
 object Constants {
+<<<<<<< HEAD
     object SharedPref {
         const val SHARED_PREF_NAME = "MySharedPref"
         private const val KEY_USERNAME = "UserName"
@@ -8,4 +9,7 @@ object Constants {
         private const val LOGOUT = "Logout"
     }
 
+=======
+    const val url = "https://team-todo-62dmq.ondigitalocean.app"
+>>>>>>> 04d613b (add api requests)
 }
