a8367c6e2c5c748b0c5159a2de30d94920385a7c
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-04-08 01:40:43 +0300

    add another requests

--- app/src/main/java/com/tahaproject/todoy_app/data/PersonalTodoRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.tahaproject.todoy_app.data
-
-data class PersonalTodoRequest(
-    val title:String?,
-    val description:String?,
-
-    )
---
+++ app/src/main/java/com/tahaproject/todoy_app/data/requests/PersonalTodoRequest.kt
@@ -0,0 +1,6 @@
+package com.tahaproject.todoy_app.data.requests
+
+data class PersonalTodoRequest(
+    val title: String?,
+    val description: String?,
+    )
