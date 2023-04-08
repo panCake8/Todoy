f7c8c7965a46fde764dd33ff3218b090cd0654b7
kareem <skrym826@gmail.com>
2023-04-08 17:26:35 +0200

    finishing UI

--- app/src/main/java/com/tahaproject/todoy_app/ui/FragmentHome.kt
+++ app/src/main/java/com/tahaproject/todoy_app/ui/FragmentHome.kt
@@ -1,5 +1,6 @@
 package com.tahaproject.todoy_app.ui
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.tahaproject.todoy_app.databinding.FragmentHomeBinding
@@ -10,5 +11,18 @@ class FragmentHome : BaseFragmentWithTransition<FragmentHomeBinding>() {
     override val bindingInflate: (LayoutInflater, ViewGroup?, Boolean) -> FragmentHomeBinding
         get() = FragmentHomeBinding::inflate
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        addCallBacks()
+    }
+
+    private fun addCallBacks(){
+        binding.viewAllTeam.setOnClickListener {
+
+
+        }
+
+
+    }
 
 }
