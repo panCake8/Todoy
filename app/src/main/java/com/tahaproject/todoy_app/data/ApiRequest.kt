58e0dc5fba46c03b6b523022c9e484cc7b60fac6
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-04-08 03:33:18 +0300

    refactor code to the best

--- app/src/main/java/com/tahaproject/todoy_app/data/ApiRequest.kt
+++ app/src/main/java/com/tahaproject/todoy_app/data/ApiRequest.kt
@@ -1,10 +1,13 @@
 package com.tahaproject.todoy_app.data
 
+import android.util.Log
 import com.google.gson.Gson
 import com.tahaproject.todoy_app.data.requests.*
 import com.tahaproject.todoy_app.data.responses.LogInResponse
+import com.tahaproject.todoy_app.data.responses.RegisterResponse
 import com.tahaproject.todoy_app.util.Constants
 import com.tahaproject.todoy_app.util.EndPoint
+import com.tahaproject.todoy_app.util.HttpMethods
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
@@ -16,55 +19,93 @@ class ApiRequest(private val gson: Gson) : IRequestApis {
     private val logInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
     }
-    private val client = OkHttpClient.Builder().addInterceptor(logInterceptor)
+    private val client = OkHttpClient.Builder().addInterceptor(logInterceptor).build()
     private val token = Constants.token
-    private fun postRequest(body: Any, endPoint: String): Request {
-        return Request
-            .Builder()
+
+
+    private fun createRequest(method: HttpMethods, body: Any?, endPoint: String): Request {
+        val requestBuilder = Request.Builder()
             .url("${Constants.url}/$endPoint")
-            .post(
-                Gson().toJson(body).toRequestBody("application/json".toMediaTypeOrNull())
-            )
             .header("Authorization", "Bearer $token")
-            .build()
+
+        when (method) {
+            HttpMethods.GET -> {
+                requestBuilder.get()
+            }
+
+            HttpMethods.POST -> {
+                requestBuilder.post(
+                    Gson().toJson(body).toRequestBody("application/json".toMediaTypeOrNull())
+                )
+            }
+
+            HttpMethods.PUT -> {
+                requestBuilder.put(
+                    Gson().toJson(body).toRequestBody("application/json".toMediaTypeOrNull())
+                )
+            }
+        }
+        return requestBuilder.build()
     }
 
-    private fun getRequest(endPoint: String): Request {
-        return Request
-            .Builder()
-            .url("${Constants.url}/$endPoint")
-            .get()
-            .header("Authorization", "Bearer $token")
-            .build()
+
+    override fun login(): LogInResponse {
+        val loginRequest = LoginRequest("", "")
+        val request = createRequest(HttpMethods.GET, loginRequest, EndPoint.login)
+        lateinit var result: LogInResponse
+        // execute the request and handle the response
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // handle the error
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.body?.string().let { jsonString ->
+                    result = gson.fromJson(jsonString, LogInResponse::class.java)
+                    Log.i(TAG_LOGIN, "$result")
+                }
+                // handle the response
+            }
+        })
+        return result
     }
 
+    override fun register(): RegisterResponse {
+        val registerRequest = RegisterRequest("", "", Constants.teamID)
+        val request = createRequest(HttpMethods.POST, registerRequest, EndPoint.signup)
+        lateinit var result: RegisterResponse
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // handle the error
+            }
 
-    override fun login() {
-        getRequest(EndPoint.login)
-
-    }
-
-    override fun register() {
-        var request =RegisterRequest("","")
-        postRequest(request, EndPoint.signup)
+            override fun onResponse(call: Call, response: Response) {
+                response.body?.string().let { jsonString ->
+                    result = gson.fromJson(jsonString, RegisterResponse::class.java)
+                    Log.i(TAG_LOGIN, "$result")
+                }
+                // handle the response
+            }
+        })
+        return result
     }
 
     override fun createPersonalTodo() {
-        var request =PersonalTodoRequest("","")
+        val request = PersonalTodoRequest("", "")
         postRequest(request, EndPoint.personalTodo)
     }
 
     override fun getPersonalTodos() {
-       getRequest(EndPoint.personalTodo)
+        getRequest(EndPoint.personalTodo)
     }
 
     override fun updatePersonalTodosStatus() {
-        var request =PersonalTodoUpdateRequest("",0)
+        val request = PersonalTodoUpdateRequest("", 0)
         postRequest(request, EndPoint.personalTodo)
     }
 
     override fun createTeamTodo() {
-        var request =TeamToDoPostRequest("","","")
+        val request = TeamToDoPostRequest("", "", "")
         postRequest(request, EndPoint.teamTodo)
     }
 
@@ -73,7 +114,11 @@ class ApiRequest(private val gson: Gson) : IRequestApis {
     }
 
     override fun updateTeamTodosStatus() {
-        var request =PersonalTodoUpdateRequest("",0)
+        val request = PersonalTodoUpdateRequest("", 0)
         postRequest(request, EndPoint.teamTodo)
+    }
+
+    companion object {
+        const val TAG_LOGIN = "Login_Tag"
     }
 }
