ec255d50872abb9ca20802d8778c647fd29dcd05
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-04-08 18:23:34 +0300

    add token and url to local properties

--- app/src/main/java/com/tahaproject/todoy_app/util/Constants.kt
+++ app/src/main/java/com/tahaproject/todoy_app/util/Constants.kt
@@ -1,9 +1,6 @@
 package com.tahaproject.todoy_app.util
 
 object Constants {
-    const val url = "https://team-todo-62dmq.ondigitalocean.app"
-    const val token =
-        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJodHRwczovL3RoZS1jaGFuY2Uub3JnLyIsInN1YiI6IjdmOGZlOTY2LWQzYjItNDk1My1iNDBkLWY0YzAwNmM1YTUwMCIsInRlYW1JZCI6ImYwOTdmOTQzLWU0YTMtNDZmNC04NDAyLTdjOWI1NjUzMzc1NiIsImlzcyI6Imh0dHBzOi8vdGhlLWNoYW5jZS5vcmcvIiwiZXhwIjoxNjgxMDczNTMyfQ.RQ2ap5KFtsZC1ienRVze8BbhtvSNRH0PV6BOPeQYrrQ"
     const val applicationJson = "application/json"
     const val auth = "Authorization"
     const val bearer = "Bearer"
