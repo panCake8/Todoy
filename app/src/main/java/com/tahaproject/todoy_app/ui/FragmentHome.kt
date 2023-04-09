85188ac115d3c0918878f0fbb5c08d4c75539f99
kareem <skrym826@gmail.com>
2023-04-10 00:07:06 +0200

    refactor UI

--- app/src/main/java/com/tahaproject/todoy_app/ui/FragmentHome.kt
+++ app/src/main/java/com/tahaproject/todoy_app/ui/FragmentHome.kt
@@ -1,14 +1,24 @@
 package com.tahaproject.todoy_app.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.tahaproject.todoy_app.databinding.FragmentHomeBinding
 import com.tahaproject.todoy_app.ui.baseview.BaseFragmentWithTransition
+import kotlin.math.log
 
 class FragmentHome : BaseFragmentWithTransition<FragmentHomeBinding>() {
 
     override val bindingInflate: (LayoutInflater, ViewGroup?, Boolean) -> FragmentHomeBinding
         get() = FragmentHomeBinding::inflate
+
+}
+
+
+fun startthreads(){
+
+//        Log.i()
+
 
 }
