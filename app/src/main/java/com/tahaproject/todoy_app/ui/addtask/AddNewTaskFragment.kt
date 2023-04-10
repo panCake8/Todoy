2a8bbffae5994c69fb3a886acd7b25474f84393f
youssef <yr925344git@gmail.com>
2023-04-10 23:12:33 +0200

    use view binding

--- app/src/main/java/com/tahaproject/todoy_app/ui/addtask/AddNewTaskFragment.kt
+++ app/src/main/java/com/tahaproject/todoy_app/ui/addtask/AddNewTaskFragment.kt
@@ -1,32 +1,34 @@
 package com.tahaproject.todoy_app.ui.addtask
 
-import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.viewbinding.ViewBinding
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.tahaproject.todoy_app.databinding.FragmentAddNewTaskBinding
-import com.tahaproject.todoy_app.ui.FragmentHome
-import com.tahaproject.todoy_app.ui.baseview.BaseFragment
 
 
-class AddNewTaskFragment : BaseBottomSheetDialogFragment() {
+class AddNewTaskFragment : BottomSheetDialogFragment() {
 
-    override fun createBinding(inflater: LayoutInflater, container: ViewGroup?): ViewBinding {
-        return FragmentAddNewTaskBinding.inflate(inflater, container, false)
-    }
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return BottomSheetDialog(requireContext(), theme)
+    private var _binding: FragmentAddNewTaskBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAddNewTaskBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+    }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
