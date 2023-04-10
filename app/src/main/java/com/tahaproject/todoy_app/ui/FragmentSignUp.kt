6c4b61e8a5b7914e56e89a569ef0508d7ce35e31
ay0o5h <aayosh553@gmail.com>
2023-04-10 17:37:32 +0300

    improve signup fragment

--- app/src/main/java/com/tahaproject/todoy_app/ui/FragmentSignUp.kt
+++ app/src/main/java/com/tahaproject/todoy_app/ui/FragmentSignUp.kt
@@ -10,55 +10,60 @@ import com.tahaproject.todoy_app.databinding.FragmentSignupBinding
 import com.tahaproject.todoy_app.ui.baseview.BaseFragment
 import com.tahaproject.todoy_app.ui.baseview.BaseFragmentWithTransition
 
-class FragmentSignUp:BaseFragment<FragmentSignupBinding>() {
+class FragmentSignUp:BaseFragmentWithTransition<FragmentSignupBinding>() {
     override val bindingInflate: (LayoutInflater, ViewGroup?, Boolean) -> FragmentSignupBinding
         get() = FragmentSignupBinding::inflate
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        goToLogin()
+        onSignUp()
 
-        addCallbacks()
+    }
+    private  fun goToLogin(){
+        binding.textViewLogin.setOnClickListener {
+            //TODO:
+        }
+    }
+    private  fun onSignUp(){
+        binding.buttonSignup.setOnClickListener {
+            validateInput()
+        }
     }
 
-      private fun  addCallbacks(){
-          binding.loginTextView.setOnClickListener {
-           //TODO:
-          }
-          val regex = Regex("^(?=.*[a-z])(?=.*[A-Z]).+\$")
-     binding.signupButton.setOnClickListener {
-         if (binding.usernameEditText.length() < 4) {
-             Toast.makeText(
-                 requireContext(),
-                 "username should be 4 characters at least",
-                 Toast.LENGTH_LONG
-             ).show()
-         }
-         if (binding.passwordEditText.length() < 8) {
-             Toast.makeText(
-                 requireContext(),
-                 "password should be 8 characters at least",
-                 Toast.LENGTH_LONG
-             ).show()
 
-         }
-         if (!regex.matches(binding.passwordEditText.toString())) {
-             Toast.makeText(
-                 requireContext(),
-                 "password should content at least one letter small and capital",
-                 Toast.LENGTH_LONG
-             ).show()
-         }
-         if (binding.passwordEditText != binding.confrimPasswordEditText) {
-             Toast.makeText(
-                 requireContext(),
-                 "password not match",
-                 Toast.LENGTH_LONG
-             ).show()
+    private fun validateInput() {
+        val regex = Regex("^(?=.*[a-z])(?=.*[A-Z]).+\$")
+        if (binding.editTextUsername.length() < 4) {
+            Toast.makeText(
+                requireContext(),
+                "username should be 4 characters at least",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        if (binding.editTextPassword.length() < 8) {
+            Toast.makeText(
+                requireContext(),
+                "password should be 8 characters at least",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        if (!regex.matches(binding.editTextPassword.toString())) {
+            Toast.makeText(
+                requireContext(),
+                "password should content at least one letter small and capital",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        if (binding.editTextPassword != binding.editTextConfirmPassword) {
+            Toast.makeText(
+                requireContext(),
+                "password not match",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
 
-         }
-
-     }
-      }
 
 }
