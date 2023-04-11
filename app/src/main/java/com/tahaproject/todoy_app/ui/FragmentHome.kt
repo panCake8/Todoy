028e51c4c9b3765fc9ab55f8b91825ad85d1d699
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-04-11 21:01:34 +0300

    change name of view to pie chart

--- app/src/main/java/com/tahaproject/todoy_app/ui/FragmentHome.kt
+++ app/src/main/java/com/tahaproject/todoy_app/ui/FragmentHome.kt
@@ -25,7 +25,7 @@ class FragmentHome : BaseFragmentWithTransition<FragmentHomeBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        ///
+
         val pieChart: PieChart = binding.pieChart
 
         pieChart.apply {
