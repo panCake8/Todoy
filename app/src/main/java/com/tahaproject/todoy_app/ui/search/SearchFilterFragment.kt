90da6a4ac0b1efe127b58ea97c2ecee25107f59f
youssef <yr925344git@gmail.com>
2023-04-12 17:55:54 +0200

    recreate search filter bottom sheet

---
+++ app/src/main/java/com/tahaproject/todoy_app/ui/search/SearchFilterFragment.kt
@@ -0,0 +1,33 @@
+package com.tahaproject.todoy_app.ui.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.tahaproject.todoy_app.databinding.FragmentSearchFilterBinding
+
+
+class SearchFilterFragment : BottomSheetDialogFragment() {
+
+    private var _binding: FragmentSearchFilterBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchFilterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
