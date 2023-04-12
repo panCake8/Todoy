c0f674925b78526bd17b069d35cc712e60febe5d
youssef <yr925344git@gmail.com>
2023-04-12 17:52:09 +0200

    add bottom sheet to the SearchFragment fragment

--- app/src/main/java/com/tahaproject/todoy_app/ui/search/SearchFragment.kt
+++ app/src/main/java/com/tahaproject/todoy_app/ui/search/SearchFragment.kt
@@ -1,11 +1,30 @@
 package com.tahaproject.todoy_app.ui.search
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import com.mindorks.editdrawabletext.DrawablePosition
+import com.mindorks.editdrawabletext.onDrawableClickListener
 import com.tahaproject.todoy_app.databinding.FragmentSearchBinding
 import com.tahaproject.todoy_app.ui.baseview.BaseFragmentWithTransition
 
 class SearchFragment:BaseFragmentWithTransition<FragmentSearchBinding>() {
     override val bindingInflate: (LayoutInflater, ViewGroup?, Boolean) -> FragmentSearchBinding
         get() = FragmentSearchBinding::inflate
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.searchBar.setDrawableClickListener(object : onDrawableClickListener {
+            override fun onClick(target: DrawablePosition) {
+                 if (target == DrawablePosition.RIGHT) {
+                     SearchFilterFragment().show(parentFragmentManager, Const.FILTER_TAG)
+                 }
+            }
+        })
+    }
+
+    object Const {
+        const val FILTER_TAG = "newFilterTag"
+    }
 }
