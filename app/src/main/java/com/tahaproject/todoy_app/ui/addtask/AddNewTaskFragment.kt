84f693ce426c66311342b36979f69eb3652b63dd
youssef <yr925344git@gmail.com>
2023-04-13 16:04:58 +0200

    fix bottom sheet styles

--- app/src/main/java/com/tahaproject/todoy_app/ui/addtask/AddNewTaskFragment.kt
+++ app/src/main/java/com/tahaproject/todoy_app/ui/addtask/AddNewTaskFragment.kt
@@ -1,15 +1,20 @@
 package com.tahaproject.todoy_app.ui.addtask
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.tahaproject.todoy_app.R
 import com.tahaproject.todoy_app.databinding.FragmentAddNewTaskBinding
 
 
 class AddNewTaskFragment : BottomSheetDialogFragment() {
 
+    override fun getTheme(): Int = R.style.BottomSheetDialogTheme
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog = BottomSheetDialog(requireContext(), theme)
 
     private var _binding: FragmentAddNewTaskBinding? = null
     private val binding get() = _binding!!
@@ -24,7 +29,6 @@ class AddNewTaskFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/tahaproject/todoy_app/ui/search/SearchFilterFragment.kt
+++ app/src/main/java/com/tahaproject/todoy_app/ui/search/SearchFilterFragment.kt
@@ -1,16 +1,23 @@
 package com.tahaproject.todoy_app.ui.search
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.tahaproject.todoy_app.R
 import com.tahaproject.todoy_app.databinding.FragmentSearchFilterBinding
 
 
 class SearchFilterFragment : BottomSheetDialogFragment() {
 
-    private var _binding: FragmentSearchFilterBinding? = null
+    override fun getTheme(): Int = R.style.BottomSheetDialogTheme
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog = BottomSheetDialog(requireContext(), theme)
+
+
+private var _binding: FragmentSearchFilterBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
