ed6edae989b252465b6aed55571b72f3e097f87c
youssef <yr925344git@gmail.com>
2023-04-14 14:41:00 +0200

    refactor add new task

--- app/src/main/java/com/tahaproject/todoy_app/ui/addtask/presenter/AddNewTaskPresenter.kt
+++ app/src/main/java/com/tahaproject/todoy_app/ui/addtask/presenter/AddNewTaskPresenter.kt
@@ -1,25 +1,28 @@
 package com.tahaproject.todoy_app.ui.addtask.presenter
 
-import com.tahaproject.todoy_app.data.apiManger.auth.login.LoginApiImpl
 import com.tahaproject.todoy_app.data.apiManger.personalTodo.PersonalTodoApiImpl
 import com.tahaproject.todoy_app.data.apiManger.teamTodo.TeamTodoApiImpl
 import com.tahaproject.todoy_app.data.domain.requests.PersonalTodoRequest
 import com.tahaproject.todoy_app.data.domain.requests.TeamTodoRequest
-import com.tahaproject.todoy_app.data.domain.responses.PersonalTodosResponse
-import com.tahaproject.todoy_app.ui.login.presenter.LoginContract
 
 class AddNewTaskPresenter : AddNewTaskContract.Presenter {
-    private var view: AddNewTaskContract.View? = null // Change val to var to allow updating the view
+    private var view: AddNewTaskContract.View? = null
     private val personalTodoApiImpl = PersonalTodoApiImpl()
-    private val teamTodoApiImpl = TeamTodoApiImpl() // Change the variable name to teamTodoApiImpl to match the class name
+    private val teamTodoApiImpl = TeamTodoApiImpl()
+
+    companion object {
+        private const val DEFAULT_ID = ""
+        private const val DEFAULT_STATUS = 0
+        private const val DEFAULT_CREATION_TIME = ""
+    }
 
     override fun addPersonalTask(title: String, description: String) {
         val personalTodo = PersonalTodoRequest.PersonalTodo(
-            id = "",
+            id = DEFAULT_ID,
             title = title,
             description = description,
-            status = 0, // for todo
-            creationTime = ""
+            status = DEFAULT_STATUS, // for todo
+            creationTime = DEFAULT_CREATION_TIME
         )
         val personalTodoRequest = PersonalTodoRequest(personalTodo, null, false)
 
@@ -36,12 +39,12 @@ class AddNewTaskPresenter : AddNewTaskContract.Presenter {
 
     override fun addTeamTask(title: String, description: String, assignee: String) {
         val teamTodo = TeamTodoRequest.TeamTodo(
-            id = "",
+            id = DEFAULT_ID,
             title = title,
             description = description,
             assignee = assignee,
-            status = 0, // for todo
-            creationTime = ""
+            status = DEFAULT_STATUS, // for todo
+            creationTime = DEFAULT_CREATION_TIME
         )
         val teamTodoRequest = TeamTodoRequest(teamTodo, null, false)
 
