b154f2244f2a50f28fd4363f426dd633388c537a
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-04-15 05:45:00 +0200

    fix api interceptor

--- app/src/main/java/com/tahaproject/todoy_app/data/apiManger/auth/login/LoginApi.kt
+++ app/src/main/java/com/tahaproject/todoy_app/data/apiManger/auth/login/LoginApi.kt
@@ -3,26 +3,27 @@ package com.tahaproject.todoy_app.data.apiManger.auth.login
 import com.tahaproject.todoy_app.data.ApiRequest
 import com.tahaproject.todoy_app.data.requests.LoginRequest
 import com.tahaproject.todoy_app.data.responses.LoginResponse
-import com.tahaproject.todoy_app.data.interceptors.LoginInterceptor
 import com.tahaproject.todoy_app.util.Constants
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.Credentials
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import okhttp3.Response
 import java.io.IOException
 
-class LoginApiImpl : ApiRequest(), ILoginApi {
+class LoginApi : ApiRequest(), ILoginApi {
     override fun login(
         loginRequest: LoginRequest,
         onSuccess: (LoginResponse) -> Unit,
         onFailed: (IOException) -> Unit
     ) {
         val credentials = Credentials.basic(loginRequest.username, loginRequest.password)
-        val loginClient = OkHttpClient.Builder().addInterceptor(LoginInterceptor(credentials))
+        val loginClient = OkHttpClient.Builder()
             .addInterceptor(logInterceptor)
             .build()
-        val request = getRequest(Constants.EndPoints.login)
+        val request = Request.Builder().url("${Constants.URL}/${Constants.EndPoints.login}")
+            .addHeader(Constants.AUTH, credentials).build()
         loginClient.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 onFailed(e)
