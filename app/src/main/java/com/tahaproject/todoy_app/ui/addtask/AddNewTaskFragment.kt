8c79998c94733914faf2faeb5453c3b289840d69
youssef <yr925344git@gmail.com>
2023-04-15 03:50:51 +0200

    finish the bottom sheet

--- app/src/main/java/com/tahaproject/todoy_app/ui/addtask/AddNewTaskFragment.kt
+++ app/src/main/java/com/tahaproject/todoy_app/ui/addtask/AddNewTaskFragment.kt
@@ -7,14 +7,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.isVisible
 import com.tahaproject.todoy_app.R
 import com.tahaproject.todoy_app.databinding.FragmentAddNewTaskBinding
 import com.tahaproject.todoy_app.ui.addtask.presenter.AddNewTaskContract
 import com.tahaproject.todoy_app.ui.addtask.presenter.AddNewTaskPresenter
 import com.tahaproject.todoy_app.ui.baseview.BaseBottomSheetDialogFragment
-import com.tahaproject.todoy_app.util.showToast
-import java.io.IOException
+
 
 class AddNewTaskFragment : BaseBottomSheetDialogFragment<FragmentAddNewTaskBinding>(), AddNewTaskContract.View {
 
@@ -93,7 +93,7 @@ class AddNewTaskFragment : BaseBottomSheetDialogFragment<FragmentAddNewTaskBindi
     }
 
     override fun showTaskAdded(task: String) {
-        showToast(task)
+        Toast.makeText(requireContext(), task, Toast.LENGTH_SHORT).show()
     }
 
     override fun showError(error: Throwable) {
--- app/src/main/java/com/tahaproject/todoy_app/ui/addtask/presenter/AddNewTaskContract.kt
+++ app/src/main/java/com/tahaproject/todoy_app/ui/addtask/presenter/AddNewTaskContract.kt
@@ -1,14 +1,15 @@
 package com.tahaproject.todoy_app.ui.addtask.presenter
 
-import java.io.IOException
 
 interface AddNewTaskContract {
     interface View {
         fun showTaskAdded(task: String)
-        fun showError(error: IOException)
+        fun showError(error: Throwable)
     }
 
     interface Presenter {
+        fun attachView(view: View)
+        fun detachView()
         fun addPersonalTask(title: String, description: String)
         fun addTeamTask(title: String, description: String, assignee: String)
     }
