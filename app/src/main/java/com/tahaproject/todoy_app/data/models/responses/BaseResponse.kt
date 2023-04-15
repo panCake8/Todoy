b7f5585344454e1cfabbc6d82bd836bbfb90c155
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-04-15 05:16:31 +0300

    edit things in responses

--- app/src/main/java/com/tahaproject/todoy_app/data/models/responses/BaseResponse.kt
+++ app/src/main/java/com/tahaproject/todoy_app/data/models/responses/BaseResponse.kt
@@ -1,24 +1,5 @@
 package com.tahaproject.todoy_app.data.models.responses
 
-import android.os.Parcelable
-import com.google.gson.annotations.SerializedName
-import kotlinx.parcelize.Parcelize
-import kotlinx.parcelize.RawValue
-
-//@Parcelize
-//class BaseResponse<T>(
-//    val value: @RawValue T,
-//    @SerializedName("message")  val message: String?,
-//    @SerializedName("isSuccess")  val isSuccess: Boolean,
-//) : Parcelable
-
-interface IBaseResponse<T> {
-    val value: T
-    val message: String?
-    val isSuccess: Boolean
-}
-
-
 class BaseResponse<T>(
     override val value: T,
     override val message: String?,
--- app/src/main/java/com/tahaproject/todoy_app/data/models/responses/loginResponse/LoginResponse.kt
+++ app/src/main/java/com/tahaproject/todoy_app/data/models/responses/loginResponse/LoginResponse.kt
@@ -1,11 +1,9 @@
 package com.tahaproject.todoy_app.data.models.responses.loginResponse
 
 import android.os.Parcelable
-import com.google.gson.annotations.SerializedName
 import com.tahaproject.todoy_app.data.models.responses.BaseResponse
 import com.tahaproject.todoy_app.data.models.responses.IBaseResponse
 import kotlinx.parcelize.Parcelize
-import kotlinx.parcelize.RawValue
 
 @Parcelize
 data class LoginResponse(
@@ -13,10 +11,3 @@ data class LoginResponse(
     override val message: String?,
     override val isSuccess: Boolean,
 ) : IBaseResponse<LoginValue> by BaseResponse(value, message, isSuccess), Parcelable
-
-
-//@Parcelize
-//data class LoginResponse(
-//    val loginValue: LoginValue,
-//    val baseResponse: BaseResponse<LoginValue>
-//) : Parcelable
