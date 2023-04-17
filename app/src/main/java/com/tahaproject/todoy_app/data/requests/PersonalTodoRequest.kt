22320171e0af3cefbad60652e1f109663b3905bd
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-04-18 01:54:06 +0200

    fix presenters

--- app/src/main/java/com/tahaproject/todoy_app/data/requests/PersonalTodoRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.tahaproject.todoy_app.data.requests
-
-data class PersonalTodoRequest(
-    val title: String?,
-    val description: String?,
-    )
--- app/src/main/java/com/tahaproject/todoy_app/data/responses/ContentRegisterResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.tahaproject.todoy_app.data.responses
-
-import com.google.gson.annotations.SerializedName
-
-data class ContentRegisterResponse(
-    @SerializedName("userId") val userId: String?,
-    @SerializedName("username") val username: String?,
-)
-
