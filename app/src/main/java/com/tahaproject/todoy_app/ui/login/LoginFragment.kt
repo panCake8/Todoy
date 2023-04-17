ab099611f55135da514120a731670d66d2df4c1d
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-04-17 04:11:50 +0300

    update LoginFragment.kt

--- app/src/main/java/com/tahaproject/todoy_app/ui/login/LoginFragment.kt
+++ app/src/main/java/com/tahaproject/todoy_app/ui/login/LoginFragment.kt
@@ -3,35 +3,56 @@ package com.tahaproject.todoy_app.ui.login
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+
 import com.tahaproject.todoy_app.databinding.FragmentLoginBinding
-import com.tahaproject.todoy_app.ui.baseview.BaseFragmentWithTransition
-import com.tahaproject.todoy_app.ui.login.presenter.ILoginContract
+import com.tahaproject.todoy_app.ui.base.BaseFragment
 import com.tahaproject.todoy_app.ui.login.presenter.LoginPresenter
 
-class LoginFragment: BaseFragmentWithTransition<FragmentLoginBinding>() ,ILoginContract.View{
-    private val presenter =LoginPresenter()
+class LoginFragment: BaseFragment<FragmentLoginBinding,LoginPresenter>(){
+
 
     override val bindingInflate: (LayoutInflater, ViewGroup?, Boolean) -> FragmentLoginBinding
         get() = FragmentLoginBinding::inflate
+    override val presenter: LoginPresenter
+        get() = TODO("Not yet implemented")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setup()
+
+
+            onLogin()
 
     }
-    private fun setup(){
+     private fun onLogin() {
+//         val username = binding.editTextUsername.text.toString()
+//         val password = binding.editTextPassword.text.toString()
 
-        presenter.attach(this)
+
+//       presenter.fetchData(LoginRequest(username,password))
+     }
+
+//    override fun onSuccess(loginResponse: LoginResponse) {
+//        if (loginResponse.isSuccess){
+//
+//        }
+//        else showToast(INCORRECT_INPUT)
+//    }
+//
+//    override fun onFailRequest(error: IOException) {
+//        requireActivity().runOnUiThread {
+//            error.localizedMessage?.let { showToast(it) }
+//        }
+//    }
+//    override fun showInvalidMassage(usernameMassage:String,passwordMassage:String) {
+//        binding.editTextUsername.error =usernameMassage
+//        binding.editTextPassword.error =passwordMassage
+//    }
+
+    companion object {
+
+        const val INCORRECT_INPUT = "your username or password is incorrect"
     }
-    private fun addCallback() {
 
-        val username = binding.editTextUsername.text
-        val password = binding.editTextPassword.text
 
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
 }
