20172956accb8298ab0c6034c8ac0ef0eb7871a2
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-04-17 04:12:04 +0300

    update LoginPresenter.kt

--- app/src/main/java/com/tahaproject/todoy_app/ui/login/presenter/LoginPresenter.kt
+++ app/src/main/java/com/tahaproject/todoy_app/ui/login/presenter/LoginPresenter.kt
@@ -1,22 +1,42 @@
 package com.tahaproject.todoy_app.ui.login.presenter
 
-import com.tahaproject.todoy_app.data.apiManger.auth.login.LoginApiImpl
+import com.tahaproject.todoy_app.data.apiManger.auth.login.ILoginApi
+import com.tahaproject.todoy_app.data.apiManger.auth.login.LoginApi
+import com.tahaproject.todoy_app.data.models.requests.LoginRequest
 import com.tahaproject.todoy_app.data.models.responses.loginResponse.LoginResponse
 import java.io.IOException
 
-class LoginPresenter():ILoginContract.Presenter  {
-//    private val loginView:ILoginContract.View
-    private val api  =LoginApiImpl()
-    private var view :ILoginContract.View?=null
-    override fun getLoginResponse(loginResponse: LoginResponse) {
+
+class LoginPresenter(private val view: LoginContract.IView) : LoginContract.IPresenter {
+    private val loginRequestApiImpl: ILoginApi = LoginApi()
+    override fun fetchData(loginRequest: LoginRequest) {
+        if (validate(loginRequest)){
+            loginRequestApiImpl.login(loginRequest,::onLoginSuccess,::onLoginFailed)
+        }
+        else{
+            validate(loginRequest)
+        }
+    }
+
+    override fun onLoginSuccess(result: LoginResponse) {
+      view?.onSuccess(result)
+    }
+    override fun onLoginFailed(e: IOException) {
 
     }
 
-    override fun onFailRequest(error: IOException) {
+    override fun validate(loginRequest: LoginRequest): Boolean {
+        return if(loginRequest.username.isNullOrEmpty()){
+            view?.showInvalidMassage("please enter username","")
+            false
+        } else if(loginRequest.password.isNullOrEmpty()){
+            view?.showInvalidMassage("","please enter password")
+            false
+        } else{
+            true
+        }
 
-    }
-    fun attach(LoginView:ILoginContract.View){
-        this.view =LoginView
     }
 
 }
+
