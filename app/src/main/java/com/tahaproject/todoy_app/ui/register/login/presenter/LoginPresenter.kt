02c57229b119ddb786791145976af89633664610
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-04-18 00:31:42 +0300

    update validate function

--- app/src/main/java/com/tahaproject/todoy_app/ui/register/login/presenter/LoginPresenter.kt
+++ app/src/main/java/com/tahaproject/todoy_app/ui/register/login/presenter/LoginPresenter.kt
@@ -1,6 +1,5 @@
 package com.tahaproject.todoy_app.ui.login.presenter
 
-import androidx.fragment.app.Fragment
 import com.tahaproject.todoy_app.data.apiManger.auth.login.ILoginApi
 import com.tahaproject.todoy_app.data.apiManger.auth.login.LoginApi
 import com.tahaproject.todoy_app.data.models.requests.LoginRequest
@@ -11,33 +10,36 @@ import java.io.IOException
 class LoginPresenter(private val view: LoginContract.IView) : LoginContract.IPresenter {
     private val loginRequestApiImpl: ILoginApi = LoginApi()
     override fun fetchData(loginRequest: LoginRequest) {
-        if (isValid(loginRequest)){
-            loginRequestApiImpl.login(loginRequest,::onLoginSuccess,::onLoginFailed)
-        }
-        else{
-            if(loginRequest.username.isNullOrEmpty()){
-
-                view?.showInvalidMassage("please enter your username","")
-            }
-            else if(loginRequest.password.isNullOrEmpty()){
-                view?.showInvalidMassage("","please enter your password",)
-            }
+        if (isValid(loginRequest)) {
+            loginRequestApiImpl.login(loginRequest, ::onLoginSuccess, ::onLoginFailed)
         }
     }
+
+    private fun isValidUsername(username: String) = username.isNotEmpty()
+    private fun isValidPassword(password: String) = password.isNotEmpty()
 
     override fun onLoginSuccess(result: LoginResponse) {
-      view?.onSuccess(result)
+        view.onSuccess(result)
+        view.getToken(result.value.token)
     }
+
     override fun onLoginFailed(e: IOException) {
 
     }
 
     override fun isValid(loginRequest: LoginRequest): Boolean {
-        return loginRequest.username.isNotEmpty() &&
-                loginRequest.password.isNotEmpty()
-
+        return if (!isValidUsername(loginRequest.username)) {
+                    view.showInvalidMassage("please enter your username", "")
+                    false
+                }
+                else if (!isValidPassword(loginRequest.password)) {
+                    view.showInvalidMassage("", "please enter your password")
+                    false
+                }
+                else {
+                    true
+                }
     }
-
 
 }
 
