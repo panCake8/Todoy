e1fc7baf4af1adfab3e3b2de7d418a0f8f643ed8
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-04-17 14:32:02 +0300

    update onLogin and onSuccess functions

--- app/src/main/java/com/tahaproject/todoy_app/ui/login/LoginFragment.kt
+++ app/src/main/java/com/tahaproject/todoy_app/ui/login/LoginFragment.kt
@@ -3,6 +3,8 @@ package com.tahaproject.todoy_app.ui.login
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.commit
+import com.tahaproject.todoy_app.R
 import com.tahaproject.todoy_app.data.models.requests.LoginRequest
 import com.tahaproject.todoy_app.data.models.responses.loginResponse.LoginResponse
 
@@ -10,6 +12,7 @@ import com.tahaproject.todoy_app.databinding.FragmentLoginBinding
 import com.tahaproject.todoy_app.ui.base.BaseFragment
 import com.tahaproject.todoy_app.ui.login.presenter.LoginContract
 import com.tahaproject.todoy_app.ui.login.presenter.LoginPresenter
+import com.tahaproject.todoy_app.ui.signup.SignUpFragment
 import com.tahaproject.todoy_app.util.showToast
 import java.io.IOException
 
@@ -25,17 +28,35 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginPresenter>(), Logi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         onLogin()
+        goToSignUp()
 
     }
 
     private fun onLogin() {
-        val username = binding.editTextUsername.text.toString()
-        val password = binding.editTextPassword.text.toString()
 
-        presenter.fetchData(LoginRequest(username, password))
+        binding.loginButton.setOnClickListener {
+            val username = binding.editTextUsername.text.toString()
+            val password = binding.editTextPassword.text.toString()
+            presenter.fetchData(LoginRequest(username, password))
+
+        }
+    }
+    private fun goToSignUp(){
+        binding.textviewSignUp.setOnClickListener {
+            parentFragmentManager.commit {
+                replace(R.id.fragment_register_container,SignUpFragment())
+                addToBackStack("signup")
+                setReorderingAllowed(true)
+            }
+        }
     }
 
     override fun onSuccess(loginResponse: LoginResponse) {
+        requireActivity().runOnUiThread {
+            if (loginResponse.isSuccess) {
+                //go to home screen
+            } else showToast(SUCCESS_LOGIN)
+        }
     }
 
     override fun onFailRequest(error: IOException) {
@@ -51,6 +72,7 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginPresenter>(), Logi
 
     companion object {
         const val INCORRECT_INPUT = "your username or password is incorrect"
+        const val SUCCESS_LOGIN = "success login"
     }
 
 
