44ef866036f68ee5d63fa8f4ae1f5a327bebb048
youssef <yr925344git@gmail.com>
2023-04-18 23:20:20 +0200

    refactor home presenter

--- app/src/main/java/com/tahaproject/todoy_app/ui/home/homePresenter/HomePresenter.kt
+++ app/src/main/java/com/tahaproject/todoy_app/ui/home/homePresenter/HomePresenter.kt
@@ -1,51 +1,38 @@
 package com.tahaproject.todoy_app.ui.home.homePresenter
 
-
-import android.util.Log
 import com.tahaproject.todoy_app.data.apiManger.personalTodo.IPersonalTodoApi
 import com.tahaproject.todoy_app.data.apiManger.personalTodo.PersonalTodoApi
 import com.tahaproject.todoy_app.data.apiManger.teamTodo.ITeamTodoApi
 import com.tahaproject.todoy_app.data.apiManger.teamTodo.TeamTodoApi
 import com.tahaproject.todoy_app.data.models.responses.todosListResponse.ToDosResponse
-import com.tahaproject.todoy_app.data.models.responses.todosListResponse.Todo
 import com.tahaproject.todoy_app.ui.presenter.IHomeContract
+import java.io.IOException
 
 
-class HomePresenter(private val view: IHomeContract.IView, token: String) :
-    IHomeContract.IPresenter {
+class HomePresenter(private val view: IHomeContract.IView, token: String) : IHomeContract.IPresenter {
+
     private val personalTodoApiImpl: IPersonalTodoApi = PersonalTodoApi(token)
     private val teamTodoApi: ITeamTodoApi = TeamTodoApi(token)
 
-    lateinit var personalData: ToDosResponse
-    lateinit var teamData: ToDosResponse
-
     override fun fetchPersonalData() {
-        view.let { view ->
-            personalTodoApiImpl.getPersonalTodos({
-                personalData = it
-                Log.i("personalData", personalData.toString())
-            }, { ioException ->
-                view.showError(ioException)
-            })
-        }
+        personalTodoApiImpl.getPersonalTodos( ::onPersonalSuccess, ::onFail )
     }
 
     override fun fetchTeamData() {
-        view.let { view ->
-            teamTodoApi.getTeamTodos({
-                teamData = it
-                Log.i("teamData", teamData.toString())
-
-            }, { ioException ->
-                view.showError(ioException)
-            })
-
-
-        }
+        teamTodoApi.getTeamTodos( ::onTeamSuccess, ::onFail )
     }
 
+    private fun onTeamSuccess(todo : ToDosResponse) {
+        view.showTeamToDoData(todo)
+    }
 
+    private fun onPersonalSuccess(todo : ToDosResponse) {
+        view.showPersonalToDoData(todo)
+    }
 
+    private fun onFail(error: IOException) {
+        view.showError(error)
+    }
 
     override fun onUnauthorizedError() {
         view.navigateToLoginScreen()
